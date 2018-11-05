1989b5a68b6705387e3d9f379c551dfd924835e5
pacificsoftdevptpt <pacificsoftdevpt.pt@gmail.com>
2018-11-05 08:48:34 +0700

    init

---
+++ ngjs-intl-tel-input.js
@@ -0,0 +1,49 @@
+(function () {
+    "use strict"
+
+    angular.module("ngjsIntlTelInput", []).directive('ngjsIntlTelInput', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'E',
+            scope: {
+                ngModel: '=',
+                options: '=',
+                ngRequired: '='
+            },
+            template: '<input id="drphone" type="tel" ng-model="data" ng-required="ngRequired" ng-pattern="pattern">',
+            link: function (scope, element, attrs, ctrl) {
+
+                var input = document.querySelector("#drphone");
+
+                // initialise plugin
+                scope.options.utilsScript = '//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/14.0.4/js/utils.js';
+                var iti = window.intlTelInput(input, scope.options);
+
+                input.addEventListener('change', function () {
+                    eventFuntion();
+                });
+                input.addEventListener('keyup', function () {
+                    eventFuntion();
+                });
+                input.addEventListener('blur', function () {
+                    eventFuntion();
+                });
+                input.addEventListener("countrychange", function () {
+                    eventFuntion();
+                });
+
+                var eventFuntion = function () {
+                    if (iti.isValidNumber()) {
+                        $timeout(function () {
+                            scope.ngModel = iti.getNumber();
+                            scope.pattern = '[0-9]+';
+                        });
+                    } else {
+                        $timeout(function () {
+                            scope.pattern = '[]+';
+                        });
+                    }
+                };
+            }
+        }
+    }]);
+}).call(this);
