e42833b1c9ea533d530046ca03641fa9ca49e4c9
pacificsoftdevpt <pacificsoftdev.pt@gmail.com>
2019-01-17 22:54:22 +0700

    update

--- ngjs-intl-tel-input.js
+++ ngjs-intl-tel-input.js
@@ -11,12 +11,28 @@
             },
             template: '<input id="drphone" type="tel" ng-model="data" ng-required="ngRequired" ng-pattern="pattern">',
             link: function (scope, element, attrs, ctrl) {
-
+                
                 var input = document.querySelector("#drphone");
 
                 // initialise plugin
                 scope.options.utilsScript = './utils.js';
                 var iti = window.intlTelInput(input, scope.options);
+
+                scope.$watch('ngModel', function(v) {
+                    if (scope.notWatchModel) {
+                        scope.notWatchModel = false;
+                        return;
+                    }
+                    if (v) {
+                        iti.setNumber(v);
+
+                        $timeout(function() {
+                            var value = element.find('#drphone').val();
+                            value = value.replace(/ /g, '');
+                            element.find('#drphone').val(value);
+                        }, 1000)
+                    }
+                });
 
                 input.addEventListener('change', function () {
                     eventFuntion();
@@ -34,6 +50,7 @@
                 var eventFuntion = function () {
                     if (iti.isValidNumber()) {
                         $timeout(function () {
+                            scope.notWatchModel = true;
                             scope.ngModel = iti.getNumber();
                             scope.pattern = '[0-9]+';
                         });
